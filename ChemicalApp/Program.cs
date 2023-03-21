e6750abcdbc0ebb4d346aeb8d1563c7d6803d6f4
Bailey Pollard <baipoll@chbc.school.nz>
2023-03-21 13:18:11 +1300

    -Added structual comments to program -Begun coding for chemical name and displaying it

--- ChemicalApp/Program.cs
+++ ChemicalApp/Program.cs
@@ -4,9 +4,43 @@ namespace ChemicalApp
 {
     class Program
     {
+
+        //Global variables
+        //methods and/or functions
+
+
+
+        static void OneChemical()
+        {
+            //Enter and store chemical name
+            Console.WriteLine("Enter a chemical for testing:\n");
+            string chemicalName = Console.ReadLine();
+
+            //Display chemical
+            Console.WriteLine(chemicalName);
+
+            //Loop 5 times
+            for (int chemicalTest = 0; chemicalTest < 5; chemicalTest++)
+            {
+
+            }
+
+            //Randomly generate a random amount of germs
+
+            //Add chemical to amount of germs
+
+            //Display an amount of time waited for testing
+
+            //Sample the live amount of germs left after testing
+
+            //Calculate the efficiecy rating of that chemical tested
+
+            //Display each chemicals efficiency rating over the 5 tests
+
+        }
         static void Main(string[] args)
         {
-            
+            OneChemical();
         }
     }
 }
