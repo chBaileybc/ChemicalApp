8af02fe7afcabf630efbf74b670432607fbb6fa1
Bailey Pollard <baipoll@chbc.school.nz>
2023-03-27 12:27:40 +1300

    Coded- Made a list for chemical menu Added chemical to germs for efficiency rating

--- ChemicalApp/Program.cs
+++ ChemicalApp/Program.cs
@@ -8,52 +8,45 @@ namespace ChemicalApp
 
         //Global variables
 
-        
+        static List<string> chemicals = new List<string>() 
+        { "Propane", "Cyanide", "Bleach", "Hot water","Alcohol"};
 
         //methods and/or functions
 
-
-
         static void OneChemical()
         {
-            List<string> checmialChoice = new List<string>() 
-            { 
-                "Propane","Cyanide","Bleach","Hot water","Alcohol"
-            };
-
-
             //Enter and store chemical name
 
-            Console.WriteLine("Type a chemical from the either of the examples below:\n" +
+            Console.WriteLine("Enter a chemical for testing below:\n\n" +
                 "1.Propane\n" +
                 "2.Cyanide\n" +
                 "3.Bleach\n" +
-                "4.Hot water\n" +
+                "4.Hot water\n" + 
                 "5.Alcohol\n");
 
-            string chemicalName = Console.ReadLine();
+            int chemicalName = Convert.ToInt32(Console.ReadLine());
 
             //Display chemical
 
-            Console.WriteLine($"{chemicalName}\n");
+            Console.WriteLine($"\n{chemicals[chemicalName-1]}\n");
 
             //Randomly generate a random amount of germs
 
             Random randGen = new Random();
 
-            int germCount = randGen.Next(1, 7500);
+            int germCount = randGen.Next(2000, 7500);
 
-            Console.WriteLine($"There are {germCount} germs for testing");
+            Console.WriteLine($"There are {germCount} germs for testing\n");
 
             //Display an amount of time waited for testing
 
             int germTime = randGen.Next(1,30);
 
-            Console.WriteLine($"You have waited {germTime}s after using {chemicalName}\n");
+            Console.WriteLine($"You have waited {germTime}s after using {chemicals[chemicalName-1]}\n");
 
             //Add chemical to amount of germs
 
-
+            Console.WriteLine("");
 
             //Sample the live amount of germs left after testing
 
