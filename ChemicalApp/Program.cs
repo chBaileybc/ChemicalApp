eb51ed4bc9697f43eb8461e79b54930f2df3fb2c
Bailey Pollard <baipoll@chbc.school.nz>
2023-04-04 12:53:58 +1200

    Coded- Created a main method called CheckFlag() Displayed an error message after entering wrong character

--- ChemicalApp/Program.cs
+++ ChemicalApp/Program.cs
@@ -8,7 +8,7 @@ namespace ChemicalApp
 
         //Global variables
 
-        static List<string> chemicals = new List<string>() 
+        static List<string> chemicals = new List<string>()
         { "Propane", "Cyanide", "Bleach", "Hot water","Alcohol"};
 
         //methods and/or functions
@@ -21,18 +21,18 @@ namespace ChemicalApp
                 "1.Propane\n" +
                 "2.Cyanide\n" +
                 "3.Bleach\n" +
-                "4.Hot water\n" + 
+                "4.Hot water\n" +
                 "5.Alcohol\n");
 
             int chemicalName = Convert.ToInt32(Console.ReadLine());
 
             //Display chemical
 
-            Console.WriteLine($"\n{chemicals[chemicalName-1]}\n");
-
-            float sumEfficiency = 0;
+            Console.WriteLine($"\n{chemicals[chemicalName - 1]}\n");
 
             //Loop 5 times
+
+            float sumEfficiency = 0;
 
             for (int chemicalTest = 0; chemicalTest < 5; chemicalTest++)
             {
@@ -42,18 +42,17 @@ namespace ChemicalApp
 
                 int germCount = randGen.Next(2000, 2500);
 
-                Console.WriteLine($"There are {germCount} germs for testing\n");
+                Console.WriteLine($"There are {germCount} germs\n");
 
                 //Display an amount of time waited for testing
 
-                int germTime = randGen.Next(10, 15);
+                int germTime = randGen.Next(5, 10);
 
                 Console.WriteLine($"You have waited {germTime}s after using {chemicals[chemicalName - 1]} on the germs\n");
 
                 int afterGerm = randGen.Next(1000, 1500);
 
                 Console.WriteLine($"There are {afterGerm} germs remaining\n");
-                
 
                 //Add chemical to amount of germs
 
@@ -75,11 +74,30 @@ namespace ChemicalApp
 
 
         }
+
+        static void CheckFlag()
+        {
+            while (true)
+            {
+                //Get users choice
+
+                //Display error message
+            }
+        }
+        
+        
         static void Main(string[] args)
         {
             OneChemical();
 
             //Loop(While) OneChemical() until user types "XXX" or "xxx"
+
+            string flagMain = "";
+
+            while (!flagMain.Equals("XXX"))
+            {
+                flagMain = CheckFlag();
+            }
 
             //Determine the chemicals with the highest rating and lowest rating
 
